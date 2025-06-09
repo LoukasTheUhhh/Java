a4a589d2946d1123d6518933cbedef7c2beebd36
Daved <Cazamirloucas@gmail.com>
2025-06-09 17:09:40 +0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+/*Just an hello world program,nothing special.*/
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
